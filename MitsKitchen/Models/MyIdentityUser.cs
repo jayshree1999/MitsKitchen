921af40af9704f7aa03b56873bc41dd3c00adc65
jayshree1999 <jayshreesolanki199@gmail.com>
2022-03-01 15:33:18 +0530

    Added MyIdentityUser and MyIdentityRole class to Model

---
+++ MitsKitchen/Models/MyIdentityUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+namespace MitsKitchen.Models
+{
+    public class MyIdentityUser
+        :IdentityUser<Guid>
+    {
+        [Display(Name = "Display Name")]
+        [Required]
+        [MinLength(3)]
+        [StringLength(60)]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Date of birth")]
+        [PersonalData]
+        [Required]
+        [Column(TypeName = "smalldatetime")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Is Admin User")]
+        [Required]
+        public Boolean IsAdminUser { get; set; }
+
+    }
+}
