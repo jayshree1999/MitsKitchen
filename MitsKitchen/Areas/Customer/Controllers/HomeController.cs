c097788ea5d1ea3627a0736ccc8f2b45e8135436
jayshree1999 <jayshreesolanki199@gmail.com>
2022-03-04 11:28:40 +0530

    product at home page

--- MitsKitchen/Areas/Customer/Controllers/HomeController.cs
+++ MitsKitchen/Areas/Customer/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MitsKitchen.Data;
+using System.Linq;
 
 namespace MitsKitchen.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+            
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Product.Include(p => p.CategoryName).ToList());
         }
         public IActionResult Privacy()
         {
